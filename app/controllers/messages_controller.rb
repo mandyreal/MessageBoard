37a7bda35138546fd114123207976ead0e00e101
Mandy Real <normandy.real@gmail.com>
2017-06-21 21:39:38 +1000

    Added find_message method

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,10 +1,11 @@
 class MessagesController < ApplicationController	
+	before_action :find_message, only: [:show, :edit, :update, :destroy]
+
 	def index
 		@messages = Message.all.order('created_at DESC')
 	end
 
 	def show
-		@message = Message.find(params[:id])
 	end
 
 	def new
@@ -24,5 +25,9 @@ class MessagesController < ApplicationController
 
 	def message_params 
 		params.require(:message).permit(:title, :description)
+	end
+
+	def find_message
+		@message = Message.find(params[:id])
 	end
 end
